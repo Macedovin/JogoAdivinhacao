3b985e1672c9dbeec9cb4e364e3b851465319ed1
Vinicius Macedo (VMP Ilustra) <93549002+VMPILUSTRA@users.noreply.github.com>
2022-08-24 10:57:44 -0300

    Deixando a aplicação mais dinâmica
    Adicionando variáveis de captação a elementos relacionadas a alertas ao usuário, refatorando parte do código e adicionando os alertas que são:
    
    - Se não houver número inserido;
    - Se o número inserido estiver fora do range proposto;
    - Se o número é baixo ou alto em relação ao número gerado;
    - Alertar corretamente, se o acerto for em somente uma tentativa;
    - Game over, caso o número de tentativas seja maior que o estipulado.

--- adivinhacao_main.js
+++ adivinhacao_main.js
@@ -16,6 +16,8 @@ const btnTry = document.querySelector('#btnTry')
 
 const btnReset = document.querySelector('#btnReset')
 
+// Capturando e reagindo ao clique da tecla ENTER
+
 document.addEventListener('keydown', function (e) {
   const isEnterKey = e.key == 'Enter'
 
@@ -25,6 +27,18 @@ document.addEventListener('keydown', function (e) {
     handleResetClick()
   }
 })
+
+/* ADICIONANDO VARIÁVEIS DE ALERTA */
+
+const alerting = document.querySelector('.alerting')
+
+const lastResults = document.querySelector('.lastResults')
+
+const triedNumbers = lastResults.querySelector('span')
+
+// refatorando um elemento utilizado repetidas vezes
+
+const screenTwoText = screenTwo.querySelector('h2'
 
 // eventos
 
@@ -43,13 +57,85 @@ function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector('#inputNumber')
+  
+  /* ADICIONANDO VARIÁVEIS CONDICIONAIS NO ESCOPO DA FUNÇÃO */
 
+  const removeClassHide = alerting.classList.remove('hide')
+
+  const notANumber = !inputNumber.value
+
+  const isInvalidNumber = inputNumber.value < 0 || inputNumber.value > 10
+
+  const isLowerNumber = inputNumber.value < randomNumber
+
+  const isHigherNumber = inputNumber.value > randomNumber
+  
+  /* ADICIONANDO CONDIÇÕES */
+
+  if (notANumber) {
+    alerting.innerText = `Tente um número!`
+
+    console.log('Sem número')
+
+    return removeClassHide
+  }
+
+  if (isInvalidNumber) {
+    alerting.innerText = `Tente um número válido!`
+
+    inputNumber.value = ''
+
+    console.log('Número inválido')
+
+    return removeClassHide
+  }
+
+  if (isLowerNumber) {
+    alerting.innerText = `Número tentado foi baixo!
+    Tente novamente!`
+
+    lastResults.classList.remove('hide')
+
+    removeClassHide
+
+    triedNumbers.textContent += `${inputNumber.value} `
+
+    console.log('Número baixo')
+  }
+
+  if (isHigherNumber) {
+    alerting.innerText = `Número tentatdo foi alto!
+    Tente novamente!`
+
+    lastResults.classList.remove('hide')
+
+    removeClassHide
+
+    triedNumbers.textContent += `${inputNumber.value} `
+
+    console.log('Número alto')
+  }
+  
+  /* ADICIONANDO CONDICIONAL DE GAME OVER */
+
+  if (inputNumber.value != randomNumber && xAttempts == 5) {
+    toggleScreen()
+
+    screenTwoText.innerText = `GAME OVER!`
+
+    console.log('Máximo de tentativas')
+  }
+  
   if (Number(inputNumber.value) == randomNumber) {
     toggleScreen()
 
-    screenTwo.querySelector(
-      'h2'
-    ).innerText = `Acertou em ${xAttempts} tentativas`
+    /* ADICIONANDO TEXTO ALTERNATIVO PARA ACERTO EM UMA TENTATIVA E REFATORANDO */
+
+    if (xAttempts > 1) {
+      screenTwoText.innerText = `Acertou em ${xAttempts} tentativas`
+    } else {
+      screenTwoText.innerText = `Acertou em ${xAttempts} tentativa!`
+    }
   }
 
   inputNumber.value = ''
@@ -60,6 +146,14 @@ function handleTryClick(event) {
 function handleResetClick() {
   toggleScreen()
 
+  /* ADICIONANDO RESET DE NOVOS ELEMENTOS */
+
+  alerting.classList.add('hide')
+
+  lastResults.classList.add('hide')
+
+  triedNumbers.innerText = ''
+  
   getRandomNumber()
 
   xAttempts = 1
